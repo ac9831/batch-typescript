df414afd865a88ff27a6819e7619210980a55fd3
Lee GunJun <ac9832@naver.com>
2022-12-01 00:31:19 +0900

    forever 추가

--- users/src/getDataPromise.ts
+++ users/src/getDataPromise.ts
@@ -5,7 +5,7 @@ type GetDataPromiseCallback = (a: IUser[]) => void
 type GetUnsplashPromiseCallback = (a: string[]) => void
 const api = process.env.REACT_APP_UNSPLASH_API
 export const getDataPromise = (fn: GetDataPromiseCallback) => (skip:number, limit: number) => {
-  fetch(`https://127.0.0.1:4000/${skip}/${limit}`)
+  fetch(`http://3.39.8.206:4000/${skip}/${limit}`)
     .then(res => res.json())
     .then(fn)
 }
